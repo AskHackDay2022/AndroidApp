3688b67cf75ecb1e4c1368a04b05651e04788213
Дамир Хамитов <hamitov.damir3101@icloud.com>
2022-06-17 17:34:46 +0600

    Last commit

--- app/src/main/java/com/damirka/androidapp/MainActivity.java
+++ app/src/main/java/com/damirka/androidapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.damirka.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.damirka.androidapp.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView LoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoginButton = findViewById(R.id.PersonImageView);
+        LoginButton.setOnClickListener(view ->
+        {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
+
 }
